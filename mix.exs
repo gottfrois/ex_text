6727185d4304bc13357dbf743d9f1c878abbe11e
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2017-02-04 17:02:45 +0100

    Adds ExDoc

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule ExText.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/gottfrois/ex_text",
      deps: deps()]
   end
 
@@ -30,7 +31,7 @@ defmodule ExText.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, "~> 0.14", only: :dev}]
   end
 
   defp package do
